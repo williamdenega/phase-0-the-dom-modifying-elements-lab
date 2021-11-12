66f9a4fec9ecf37e53e3d98de106f905d310d2ba
WilliamDenega <williamdenega@gmail.com>
2021-11-12 17:01:52 -0500

    Completed Assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+//newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
+
+newHeader.textContent = "YOUR-NAME is the champion";
+
